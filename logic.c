3168955db682658fdef706cf3ef9d18be825b64d
PUG <kjl2875@gmail.com>
2019-05-31 20:50:26 +0900

    (no comment)

--- logic.c
+++ logic.c
@@ -1,9 +1,11 @@
 // https://www.onlinegdb.com/online_c_compiler
 
 #include <stdio.h>
+#include <time.h>
 
 #define FIELD_WIDTH (3)
 #define FIELD_HEIGHT (4)
+#define CELLTYPE_MAXVAL (3)
 
 enum CellType {
     EMPTY,
@@ -12,47 +14,88 @@ enum CellType {
     URANIUM_CELL_QUAD
 };
 
-enum CellType *FIELD;
-
-void initField(void);
+void initField(enum CellType *FIELD);
+void printField(enum CellType *FIELD);
+void getNextField(enum CellType *FIELD);
 
 int main()
 {
-    int x, y;
+    clock_t t0,t1,t2;
+    enum CellType *FIELD;
     
     /// FIELD init
     
     FIELD = malloc(sizeof(enum CellType) * FIELD_WIDTH * FIELD_HEIGHT);
+    initField(FIELD);
+    
+    //printField(FIELD);
+    //printf("----------------------\n");
+    t0 = clock();
+    for( int i=0; i<16777215; i++ ) { // 4^12-1=16777215
+        getNextField(FIELD);
+        //printField(FIELD);
+        //printf("----------------------\n");
+    }
+    t1 = clock();
+    //printf("----------------------\n");
+    //printField(FIELD);
+    
+    free(FIELD);
+    
+    /// return
+    
+	printf("runtime : %.8lf \n", (((double)t1 - t0)/CLOCKS_PER_SEC));
+    
+    return 0;
+}
+
+void initField(enum CellType *FIELD)
+{
+    int x, y;
     
     for( y=0; y<FIELD_HEIGHT; y++ )
     {
         for( x=0; x<FIELD_WIDTH; x++ )
         {
-            switch( x )
-            {
-            case 2:
-                switch( y )
-                {
-                case 2:
-                    FIELD[y*FIELD_WIDTH+x] = URANIUM_CELL_DOUBLE;
-                    break;
-                }
-                break;
-            default:
-                FIELD[y*FIELD_WIDTH+x] = EMPTY;
-                break;
-            }
-            
-            printf("%d\t", FIELD[y*FIELD_WIDTH+x]);
+            FIELD[y*FIELD_WIDTH+x] = EMPTY;
         }
-        printf("\n");
     }
+}
+
+void printField(enum CellType *FIELD)
+{
+    int x, y;
     
-    /// constructor
+    for( y=0; y<FIELD_HEIGHT; y++ )
+    {
+        for( x=0; x<FIELD_WIDTH; x++ )
+        {
+            //printf("%d\t", FIELD[y*FIELD_WIDTH+x]);
+            printf(" %2d", FIELD[y*FIELD_WIDTH+x]);
+        }
+        //printf("\n");
+    }
+    printf("\n");
+}
+
+void getNextField(enum CellType *FIELD)
+{
+    int x=0,y=0;
     
-    free(FIELD);
-    
-    /// return
+    while( ++FIELD[y*FIELD_WIDTH+x] > CELLTYPE_MAXVAL )
+    {
+        FIELD[y*FIELD_WIDTH+x] = 0;
+        
+        if( ++x == FIELD_WIDTH )
+        {
+            x = 0;
+            if( ++y == FIELD_HEIGHT )
+            {
+                y = 0;
+                return 1;
+            }
+        }
+    }
     
     return 0;
 }
