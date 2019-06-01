240c25406684b3932f19004db54cbdd33ea73c8c
PUG <kjl2875@gmail.com>
2019-06-01 10:59:54 +0900

    (no comment)

--- logic.c
+++ logic.c
@@ -16,35 +16,38 @@ enum CellType {
 
 void initField(enum CellType *FIELD);
 void printField(enum CellType *FIELD);
-void getNextField(enum CellType *FIELD);
+void setNextField(enum CellType *FIELD);
+void setFieldEU(enum CellType *FIELD, enum CellType *FIELD_EU);
 
 int main()
 {
-    clock_t t0,t1,t2;
+    // INPUT
+    
+    clock_t t0,t1;
     enum CellType *FIELD;
+    enum CellType *FIELD_EU;
     
-    /// FIELD init
-    
-    FIELD = malloc(sizeof(enum CellType) * FIELD_WIDTH * FIELD_HEIGHT);
+    FIELD = malloc(sizeof(enum CellType) * (FIELD_WIDTH+2) * (FIELD_HEIGHT+2)); // border is not used
+    FIELD_EU = malloc(sizeof(int) * (FIELD_WIDTH) * (FIELD_HEIGHT)); // border is not used
     initField(FIELD);
     
-    //printField(FIELD);
-    //printf("----------------------\n");
+    // PROCESS
+    
     t0 = clock();
-    for( int i=0; i<16777215; i++ ) { // 4^12-1=16777215
-        getNextField(FIELD);
-        //printField(FIELD);
-        //printf("----------------------\n");
+    for( int i=0; i<16777215; i++ )
+    {
+        setNextField(FIELD);
+        setFieldEU(FIELD,FIELD_EU);
+        setNextField(FIELD_EU);
     }
     t1 = clock();
-    //printf("----------------------\n");
-    //printField(FIELD);
     
+    /// OUTPUT
+    
+    printField(FIELD);
+    printf("Runtime: %.06lf",(double)(t1-t0)/CLOCKS_PER_SEC);
     free(FIELD);
-    
-    /// return
-    
-	printf("runtime : %.8lf \n", (((double)t1 - t0)/CLOCKS_PER_SEC));
+    free(FIELD_EU);
     
     return 0;
 }
@@ -53,9 +56,9 @@ void initField(enum CellType *FIELD)
 {
     int x, y;
     
-    for( y=0; y<FIELD_HEIGHT; y++ )
+    for( y=0; y<FIELD_HEIGHT+2; y++ )
     {
-        for( x=0; x<FIELD_WIDTH; x++ )
+        for( x=0; x<FIELD_WIDTH+2; x++ )
         {
             FIELD[y*FIELD_WIDTH+x] = EMPTY;
         }
@@ -70,21 +73,20 @@ void printField(enum CellType *FIELD)
     {
         for( x=0; x<FIELD_WIDTH; x++ )
         {
-            //printf("%d\t", FIELD[y*FIELD_WIDTH+x]);
-            printf(" %2d", FIELD[y*FIELD_WIDTH+x]);
+            printf("%d\t", FIELD[(y+1)*FIELD_WIDTH+(x+1)]);
         }
-        //printf("\n");
+        printf("\n");
     }
     printf("\n");
 }
 
-void getNextField(enum CellType *FIELD)
+void setNextField(enum CellType *FIELD)
 {
     int x=0,y=0;
     
-    while( ++FIELD[y*FIELD_WIDTH+x] > CELLTYPE_MAXVAL )
+    while( ++FIELD[(y+1)*FIELD_WIDTH+(x+1)] > CELLTYPE_MAXVAL )
     {
-        FIELD[y*FIELD_WIDTH+x] = 0;
+        FIELD[(y+1)*FIELD_WIDTH+(x+1)] = 0;
         
         if( ++x == FIELD_WIDTH )
         {
@@ -98,4 +100,46 @@ void getNextField(enum CellType *FIELD)
     }
     
     return 0;
+}
+
+void setFieldEU(enum CellType *FIELD, enum CellType *FIELD_EU)
+{
+    int sum;
+    int x, y;
+    
+    for( y=0; y<FIELD_HEIGHT; y++ )
+    {
+        for( x=0; x<FIELD_WIDTH; x++ )
+        {
+            switch( FIELD[(y+1)*FIELD_WIDTH+(x+1)] )
+            {
+            case URANIUM_CELL_SINGLE:
+                sum = 5;
+                if( FIELD[(y+2)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 5; }
+                if( FIELD[(y+0)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 5; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+2)] != EMPTY ) { sum += 5; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+0)] != EMPTY ) { sum += 5; }
+                break;
+            case URANIUM_CELL_DOUBLE:
+                sum = 20;
+                if( FIELD[(y+2)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 10; }
+                if( FIELD[(y+0)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 10; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+2)] != EMPTY ) { sum += 10; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+0)] != EMPTY ) { sum += 10; }
+                break;
+            case URANIUM_CELL_QUAD:
+                sum = 60;
+                if( FIELD[(y+2)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 20; }
+                if( FIELD[(y+0)*FIELD_WIDTH+(x+1)] != EMPTY ) { sum += 20; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+2)] != EMPTY ) { sum += 20; }
+                if( FIELD[(y+1)*FIELD_WIDTH+(x+0)] != EMPTY ) { sum += 20; }
+                break;
+            default:
+                sum = 0;
+                break;
+            }
+            
+            FIELD_EU[y*FIELD_WIDTH+x] = sum;
+        }
+    }
 }
